ceb5e96557b72c9fc1a14b399c4381f0ee42015e
Feiyue-996 <mixiaoyuzh@gmail.com>
2024-11-20 22:34:35 +0300

    Add remaining project files

---
+++ pagesEN/views.py
@@ -0,0 +1,31 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+
+class HomePageView(TemplateView):
+    template_name = "en/index.html"
+
+
+class AboutUsPageView(TemplateView):
+    template_name = "en/about.html"
+
+class ApplicationsPageView(TemplateView):
+    template_name = "en/applications_before_lin.html"
+
+class ResearchPageView(TemplateView):
+    template_name = "en/research.html"
+
+# 定义视图函数
+def application_page1(request):
+    return render(request, 'en/applications/application_page1.html', {'current_page': 'page1'})
+
+def application_page2(request):
+    return render(request, 'en/applications/application_page2.html', {'current_page': 'page2'})
+
+def application_page3(request):
+    return render(request, 'en/applications/application_page3.html', {'current_page': 'page3'})
+
+
+
+
+
